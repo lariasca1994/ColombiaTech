44a8b7f326db609147e3cf39634bdbe4a8b6ca2b
Carraian <49419170+lariasca1994@users.noreply.github.com>
2024-04-15 06:54:01 -0500

    CT

--- PRCTDWFS/React/src/store.js
+++ PRCTDWFS/React/src/store.js
@@ -1,34 +1,32 @@
-// store.js
 import { configureStore } from '@reduxjs/toolkit';
-import chatReducer from './features/chatSlice';
-import authReducer from './features/authSlice';
 import numberReducer from './features/numberSlice';
 import usersReducer from './features/userSlice';
-import housesReducer from './features/houseSlice';
+import housesReducer from './features/houseSlice'
+import authReducer from './features/authSlice';
 import { apiSlice } from './features/api/apiSlice';
 import { apiHousesSlice } from './features/api/apiHousesSlice';
 import { apiColombiaSlice } from './features/api/apiColombiaSlice';
 import { apiMessageSlice } from './features/api/apiMessageSlice';
-import chatReducer from './features/chatSlice';
+
+/** Agrupamos los estados en una sola ubicacion */
 
 const store = configureStore({
-  reducer: {
-    chat: chatReducer,
-    auth: authReducer,
-    number: numberReducer,
-    users: usersReducer,
-    houses: housesReducer,
-    [apiSlice.reducerPath]: apiSlice.reducer,
-    [apiHousesSlice.reducerPath]: apiHousesSlice.reducer,
-    [apiColombiaSlice.reducerPath]: apiColombiaSlice.reducer,
-    [apiMessageSlice.reducerPath]: apiMessageSlice.reducer,
-  },
-  middleware: (getDefaultMiddleware) =>
+    reducer: {
+        [apiSlice.reducerPath]: apiSlice.reducer,
+        [apiHousesSlice.reducerPath]: apiHousesSlice.reducer,
+        [apiColombiaSlice.reducerPath]: apiColombiaSlice.reducer,
+        [apiMessageSlice.reducerPath]: apiMessageSlice.reducer,
+        number: numberReducer,
+        users: usersReducer,
+        houses: housesReducer,
+        auth: authReducer,
+    },
+    middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware()
-      .concat(apiSlice.middleware)
-      .concat(apiHousesSlice.middleware)
-      .concat(apiMessageSlice.middleware)
-      .concat(apiColombiaSlice.middleware),
-});
+        .concat(apiSlice.middleware)
+        .concat(apiHousesSlice.middleware)
+        .concat(apiMessageSlice.middleware)
+        .concat(apiColombiaSlice.middleware),
+})
 
-export default store;
+export default store; 
